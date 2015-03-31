0a460dacbcc7ad8d72c87432dcfb69ba1eeb3d28
kmjungersen <kmjungersen@gmail.com>
2015-03-31 17:56:34 -0400

    added the core classes that properly format a string for linking a CSS file or JS file (so far)

---
+++ pyheader/core.py
@@ -0,0 +1,59 @@
+import re
+
+
+class CSSFile():
+
+    def __init__(self, path):
+
+        self.path = path
+
+    def __str__(self):
+
+        base_string = '<link href="{path}" rel="stylesheet" type="text/css">'
+
+        link = base_string.format(
+            path=self.path,
+        )
+
+        return link
+
+
+class JSFile():
+
+    def __init__(self, path):
+
+        self.path = path
+
+    def __str__(self):
+
+        base_string = '<script src="{path}"></script>'
+
+        script = base_string.format(
+            path=self.path,
+        )
+
+        return script
+
+
+class DjangoStaticFileToken():
+
+    def __init__(self, *args):
+
+        pass
+
+    def __str__(self):
+
+        token = '{% load staticfile %}'
+
+        return token
+
+
+class Misc():
+
+    def __init__(self, string):
+
+        self.string = string
+
+    def __str__(self):
+
+        return self.string
