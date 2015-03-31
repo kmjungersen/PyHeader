bfd6e0ce428af21f5d061f6a113bdb621de7a5cb
kmjungersen <kmjungersen@gmail.com>
2015-03-31 17:55:54 -0400

    added setup.py and __init__.py to enable easy installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    description='pyheader!',
+    version='0.1',
+    name='pyheader',
+    packages=[
+        'pyheader',
+    ],
+)
